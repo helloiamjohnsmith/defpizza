b6d1e9edc40bd80f0d643d2cf8bb8666b40cc0df
Sergei Z <ss.zahar@gmail.com>
2020-04-04 16:37:18 +0300

    Continued creation of factories and seeders

--- database/seeds/PizzaSeeder.php
+++ database/seeds/PizzaSeeder.php
@@ -1,22 +1,26 @@
 <?php
 
+use App\Models\Ingredient;
+use App\Models\Size;
 use Illuminate\Database\Seeder;
 
 class PizzaSeeder extends Seeder
 {
     public function run()
     {
-        $ingredients = App\Models\Ingredient::all();
+        $ingredients = Ingredient::all();
+        $sizes = Size::all();
 
-        factory(App\Models\Pizza::class, 20)->create()->each(function ($pizza) use($ingredients) {
-//            $numOfIngredients = rand(3, 8);
-//
-//            while($numOfIngredients > 0) {
-//                $pizza->ingredients()->attach()
-//            }
+        factory(App\Models\Pizza::class, 20)->create()->each(function ($pizza) use ($ingredients, $sizes) {
             $pizza->ingredients()->attach(
                 $ingredients->random(rand(3, 8))->pluck('id')->toArray()
             );
+
+            $pizza->availableSizes()->attach(
+                $sizes->random(rand(1, 3))->pluck('id')->toArray()
+            );
+
+            $pizza->prices()->save(factory(App\Models\PizzaPrice::class)->make());
         });
     }
 }
