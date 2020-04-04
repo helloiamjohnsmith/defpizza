782a76a62a002775fabc7442656368185738c93d
Sergei Zakharchenko <zakharchenko@pgups.ru>
2020-04-04 11:36:09 +0300

    Relation "ingredients"

--- app/Models/Pizza.php
+++ app/Models/Pizza.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pizza extends Model
 {
-    //
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class, 'pizza_ingredients');
+    }
 }
