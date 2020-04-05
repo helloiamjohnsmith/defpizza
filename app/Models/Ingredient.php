0def0784824d0e72fd8bc990fb6ccb2182eea076
Sergei Zakharchenko <zakharchenko@pgups.ru>
2020-04-05 13:01:55 +0300

    Fixed fillable value

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-    protected $fillable;
+    protected $fillable = [];
 
     public function itemsExcludedFrom()
     {
