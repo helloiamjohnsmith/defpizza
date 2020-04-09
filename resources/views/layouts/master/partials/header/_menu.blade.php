eb932aa7a7417ecca7542bd919a73d6d23c87b08
Sergei Z <ss.zahar@gmail.com>
2020-04-09 15:44:19 +0300

    Links are hided for now

--- resources/views/layouts/master/partials/header/_menu.blade.php
+++ resources/views/layouts/master/partials/header/_menu.blade.php
@@ -3,12 +3,12 @@
         <li class="nav-item">
             <a class="nav-link dp-nav-link disabled" href="/" aria-disabled="true">{{ __('Menu') }}</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link dp-nav-link" href="#">{{ __('Promo') }}</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link dp-nav-link" href="#">{{ __('Contacts') }}</a>
-        </li>
+        {{--        <li class="nav-item">--}}
+        {{--            <a class="nav-link dp-nav-link" href="#">{{ __('Promo') }}</a>--}}
+        {{--        </li>--}}
+        {{--        <li class="nav-item">--}}
+        {{--            <a class="nav-link dp-nav-link" href="#">{{ __('Contacts') }}</a>--}}
+        {{--        </li>--}}
         @guest
             <li class="nav-item">
                 <a class="nav-link dp-nav-link" href="{{ route('login') }}"><i data-feather="log-in"></i></a>
