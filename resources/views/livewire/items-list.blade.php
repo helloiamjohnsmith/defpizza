448195fd59988edee4c1fbd6eb962946b51fee82
Sergei Z <ss.zahar@gmail.com>
2020-04-09 15:45:14 +0300

    Action buttons are deleted for now

--- resources/views/livewire/items-list.blade.php
+++ resources/views/livewire/items-list.blade.php
@@ -1,38 +1,37 @@
 <div>
-    <div class="d-flex justify-content-between">
-        <div class="">
-            <h2>Your shopping cart</h2>
+    @empty($items)
+        <div class="alert alert-info dp-alert">
+            Your shoppping cart is empty. <a href="/">Let's start</a> pizza shopping!
         </div>
-    </div>
+    @else
+        <div class="d-flex justify-content-between">
 
-    <div class="cart-items">
-        @foreach($items as $item)
-            <div class="cart-item">
-                <div class="cart-item__image">
-                    <img width="65px" src="{{ asset('/images/pizzas') . '/' . $item['image']}}">
-                </div>
-                <div class="cart-item__info">
-                    <div class="cart-item__title">{{ $item['title'] }}</div>
-                    <div class="cart-item__size">{{ $item['size'] }} cm</div>
-                </div>
-                <div class="cart-item__actions">
-                    <button class="cart-item__button" wire:key="x-{{ $loop->index }}" wire:click="decrease('{{  $item['id']}}')">
-                        <i data-feather="minus-circle"></i>
-                    </button>
-                    <div class="cart-item__quantity">3</div>
-
-                    <button class="cart-item__button" wire:click="increase('{{ $item['id'] }}')">
-                        <i data-feather="plus-circle"></i>
-                    </button>
-                </div>
-                <div class="cart-item__price">{{ $item['price'] }}&nbsp;&euro;</div>
-                <div class="cart-item__delete">
-                    <div wire:click="delete('{{ $item['id'] }}')">DEL</div>
-                    <button class="cart-" wire:click="delete('{{ $item['id'] }}')">
-                        <i data-feather="x-circle"></i>
-                    </button>
-                </div>
+            <div class="">
+                <h2>Your shopping cart</h2>
             </div>
-        @endforeach
-    </div>
+        </div>
+
+        <div class="cart-items">
+            @foreach($items as $item)
+                <div class="cart-item">
+                    <div class="cart-item__image">
+                        <img width="65px" src="{{ asset('/images/pizzas') . '/' . $item['image']}}">
+                    </div>
+                    <div class="cart-item__info">
+                        <div class="cart-item__title">{{ $item['title'] }}</div>
+                        <div class="cart-item__size">{{ $item['size'] }} cm</div>
+                    </div>
+                    <div class="cart-item__actions">
+
+                    </div>
+                    <div class="cart-item__price">{{ $item['price'] }}&nbsp;&euro;</div>
+                    <div class="cart-item__delete">
+                        <button class="cart-item__button" wire:click="delete('{{ $item['id'] }}')">
+                            <i data-feather="x-circle"></i>
+                        </button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endempty
 </div>
